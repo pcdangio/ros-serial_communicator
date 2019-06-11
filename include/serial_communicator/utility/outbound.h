9920637d8dddbd092f9c0c0f1d2fbcac7ce778fe
Paul D'Angio <pcdangio@gmail.com>
2019-06-11 15:43:17 -0400

    Completion of outbound implementation.

--- include/serial_communicator/utility/outbound.h
+++ include/serial_communicator/utility/outbound.h
@@ -6,6 +6,8 @@
 #include "serial_communicator/message.h"
 #include "serial_communicator/message_status.h"
 
+#include <chrono>
+
 namespace serial_communicator {
 namespace utility {
 ///
@@ -25,7 +27,55 @@ public:
     outbound(message* message, unsigned int sequence_number, bool receipt_required, message_status* tracker);
     ~outbound();
 
+    // METHODS
+    ///
+    /// \brief mark_transmitted Instrucst the outgoing message that it has been transmitted.
+    /// \details Call this method any time the message is transmitted.  It informs the instance
+    /// to update counters and timestamps related to retransmission.
+    ///
+    void mark_transmitted();
+    ///
+    /// \brief update_tracker Updates the tracker to a new message status.
+    /// \param status The new status to set.
+    ///
+    void update_tracker(message_status status);
+    ///
+    /// \brief timeout_elapsed Checks if a specified timeout has elapsed since the message was last transmitted.
+    /// \param timeout The length of the timeout period in milliseconds.
+    /// \return TRUE if the timeout has elapsed, otherwise FALSE.
+    ///
+    bool timeout_elapsed(unsigned int timeout) const;
+    ///
+    /// \brief can_retransmit Checks if the message can be retransmitted, or if it has reached its max transmissions.
+    /// \param transmit_limit The maximum allowed transmissions of the message.
+    /// \return TRUE if the message may be retransmitted, otherwise FALSE.
+    ///
+    bool can_retransmit(unsigned char transmit_limit) const;
+
+    // PROPERTIES
+    ///
+    /// \brief p_message Gets a reference to the current outbound message.
+    /// \return A const reference to the current outbound message.
+    ///
+    const message* p_message() const;
+    ///
+    /// \brief p_sequence_number Gets the originating sequence number of the outbound message.
+    /// \return The originating sequence number of the outbound message.
+    ///
+    unsigned int p_sequence_number() const;
+    ///
+    /// \brief p_receipt_required Gets if the message is requiring a receipt from the receiver or not.
+    /// \return TRUE if receipt is required, otherwise FALSE.
+    ///
+    bool p_receipt_required() const;
+    ///
+    /// \brief p_n_transmissions Gets the total number of transmissions of the message.
+    /// \return The total number of transmissions of the message.
+    ///
+    unsigned char p_n_transmissions() const;
+
 private:
+    // VARIABLES
     ///
     /// \brief m_message Stores the outgoing message.
     ///
@@ -42,6 +92,14 @@ private:
     /// \brief m_tracker Stores a pointer to the tracker for external observation of the outgoing message's status.
     ///
     message_status* m_tracker;
+    ///
+    /// \brief m_transmit_timestamp Stores the last time in which the message was transmitted.
+    ///
+    std::chrono::high_resolution_clock::time_point m_transmit_timestamp;
+    ///
+    /// \brief m_n_transmissions Stores the total number of times the message has been transmitted.
+    ///
+    unsigned char m_n_transmissions;
 };
 }}
 
--- src/outbound.cpp
+++ src/outbound.cpp
@@ -5,8 +5,63 @@ using namespace serial_communicator::utility;
 
 outbound::outbound(message* message, unsigned int sequence_number, bool receipt_required, message_status* tracker)
 {
+    // Store locals.
     outbound::m_message = message;
     outbound::m_sequence_number = sequence_number;
     outbound::m_receipt_required = receipt_required;
     outbound::m_tracker = tracker;
+
+    // Initialize counters.
+    outbound::m_transmit_timestamp = std::chrono::high_resolution_clock::now();
+    outbound::m_n_transmissions = 0;
+
+    // Set tracker status to queued.
+    outbound::update_tracker(message_status::QUEUED);
+}
+outbound::~outbound()
+{
+    delete outbound::m_message;
+}
+
+// METHODS
+void outbound::mark_transmitted()
+{
+    // Update transmission timestamp.
+    outbound::m_transmit_timestamp = std::chrono::high_resolution_clock::now();
+    // Increment transmission counter.
+    outbound::m_n_transmissions++;
+}
+void outbound::update_tracker(message_status status)
+{
+    if(outbound::m_tracker)
+    {
+        *outbound::m_tracker = status;
+    }
+}
+bool outbound::timeout_elapsed(unsigned int timeout) const
+{
+    // Check if the given timeout has been elapsed.
+    return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - outbound::m_transmit_timestamp).count() > timeout;
+}
+bool outbound::can_retransmit(unsigned char transmit_limit) const
+{
+    return outbound::m_n_transmissions < transmit_limit;
+}
+
+// PROPERTIES
+const message* outbound::p_message() const
+{
+    return outbound::m_message;
+}
+unsigned int outbound::p_sequence_number() const
+{
+    return outbound::m_sequence_number;
+}
+bool outbound::p_receipt_required() const
+{
+    return outbound::m_receipt_required;
+}
+unsigned char outbound::p_n_transmissions() const
+{
+    return outbound::m_n_transmissions;
 }
